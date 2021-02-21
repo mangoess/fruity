8a7c81a4283d9312dd7cfbb8f122c3a9d98fcb8e
mango <adamtamers@outlook.com>
2021-02-21 12:39:42 +0400

    Update fruit.py

--- fruit.py
+++ fruit.py
@@ -3,10 +3,31 @@ import time
 import json
 
 def fruit(fruit):
-  x = requests.get('https://fruityvice.com/api/fruit/{}'.format(fruit)
+  x = requests.get('https://fruityvice.com/api/fruit/{}'.format(fruit) )
   print("Status Code", x.status_code)
   print(x)
   
+  
+def fruit(element):
+  y = requests.get('https://fruityvice.com/api/fruit/{}'.format(fruit) )
+  data = (y.text)
+  json_object = json.loads(x.text)
+  print(json_object["{}"].format(element) )
+  
+  
+  
+  
+
+#x = requests.get('https://fruityvice.com/api/fruit/(fruit)')
+#print("\n", x.text)
+
+#load the data into an element
+#data = (x.text)
+
+#json_object = json.loads(x.text)
+
+#print(json_object["genus"])
+
   # GETTING INFORMATION OFF : https://fruityvice.com/api/fruit/(fruit)
   # REQUEST TYPE : GET
   # REQUEST FOR ALL FRUITS : https://fruityvice.com/api/fruit/all
