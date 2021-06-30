417b1b55a4d3a2643cd17ec85721e9eed04ba799
mango <adamtamers@outlook.com>
2021-06-30 08:00:36 +0400

    Update fruit.py

--- fruit.py
+++ fruit.py
@@ -2,30 +2,23 @@ import requests
 import time
 import json
 
-def fr(fruit):
+def getfruitname(fruit):
   x = requests.get('https://fruityvice.com/api/fruit/{}'.format(fruit) )
   print("Status Code", x.status_code)
-  print(x.text)
+  fruit = x.text
   
   
-def el(element):
+def getfruitelement(element):
   y = requests.get('https://fruityvice.com/api/fruit/{}'.format(fruit) )
   data = (y.text)
   json_object = json.loads(y.text)
   print(json_object["{}"].format(element) )
-
-#x = requests.get('https://fruityvice.com/api/fruit/(fruit)')
-#print("\n", x.text)
-
-#load the data into an element
-#data = (x.text)
-
-#json_object = json.loads(x.text)
-
-#print(json_object["genus"])
+json_object["genus"])
 
   # GETTING INFORMATION OFF : https://fruityvice.com/api/fruit/(fruit)
   # REQUEST TYPE : GET
   # REQUEST FOR ALL FRUITS : https://fruityvice.com/api/fruit/all
   
   #THIS IS A TEST BRANCH, NOT INTENDED FOR USE YET.
+
+  
